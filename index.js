b1666dbe230bafcadcbc8bf1f2e8ae85dec5647b
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-08-03 01:38:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.set("view engine", "ejs")
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+
+})
+app.post("/output.html", function (req, res) {
+    var age = req.body.age;
+    lifeInDays = (90 * 365) - (age * 365);
+    lifeInWeeks = (90 * 52) - (age * 52);
+    lifeInMonths = (90 * 12) - (age * 12);
+    res.render("output", { Days: lifeInDays, Weeks: lifeInWeeks, Months: lifeInMonths, age: age })
+    console.log(req.body)
+})
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port 3000")
+})
